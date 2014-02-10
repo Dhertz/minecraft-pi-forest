6be199e791a625586f9611c6eb42b8a021f1fe95
Daniel Hertz <daniel@dhertz.com>
2014-02-10 10:46:23 +0000

    Set up the Pis for class

---
+++ config.sh
@@ -0,0 +1,15 @@
+sudo /sbin/iptables -A OUTPUT -p udp --source-port 19132 -j DROP
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+
+sudo apt-get install -y git nodejs npm luakit iptables-persistent
+
+sudo su -c 'iptables-save > /etc/iptables/rules.v4'
+
+git clone git://github.com/Dhertz/RPi-WaterbearNodeJS
+cd RPi-WaterbearNodeJS
+git submodule update --init --recursive
+npm install --registry http://registry.npmjs.org
+/home/pi/mcpi/minecraft-pi &
+nodejs node-js-server.js &
+luakit -u http://localhost:8000/minecraftjs.html
