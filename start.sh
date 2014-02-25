16b14e934e74e04597fa1989899e2d9b3c6511b8
Daniel Hertz <daniel@dhertz.com>
2014-02-25 20:48:48 +0000

    update start.sh to update everything before running it

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git pull
+cd /home/pi/RPi-WaterbearNodeJS
+git pull
+git submodule update
 nodejs nodejs-server.js &
 /home/pi/mcpi/minecraft-pi &
 luakit -u http://localhost:8000/minecraftjs.html
