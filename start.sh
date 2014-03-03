c26cbb32151f2fb8bf7d934aa4ae97bd4df46c0d
Daniel Hertz <daniel@dhertz.com>
2014-03-03 21:35:00 +0000

    Correct URL

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ git submodule update
 nodejs nodejs-server.js &
 /home/pi/mcpi/minecraft-pi &
 IP=`ip addr show eth0 | grep -oP '(inet [\d|\.]+)' | grep -oP '[\d|\.]+'`
-echo "Go to http://$IP:8000/minecraftjs.html:wq on the RM computer in chrome!"
+echo "Go to http://$IP:8000/minecraftjs.html on the RM computer in chrome!"
 
