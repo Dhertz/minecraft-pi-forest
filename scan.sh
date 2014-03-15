c0b6fce9659215879854c8dddd2b5121b01fa83d
Daniel Hertz <daniel@dhertz.com>
2014-03-15 14:02:04 +0000

    added required dependencies

--- scan.sh
+++ scan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install nmap
+sudo apt-get -y install nmap sshpass --no-install-recommends
 mesg y
-nmap -p 22 10.210.193.0/24 | grep -B 3 open | grep Nmap | grep -oP '[\d|\.]+'
+nmap -p 22 192.168.1.0/24 | grep -B 3 open | grep Nmap | grep -oP '[\d|\.]+.\d'
 
